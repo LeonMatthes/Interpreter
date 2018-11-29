97be86e2abc4707c156c11d421a85c903c61de7b
Leon <leon@matthes.biz>
2018-11-29 01:42:47 +0100

    Debug requiring

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-module.exports = require("bindings")("InterpreterNan");
+const path = require('path');
+const requireFunction = typeof __webpack_require__ === 'function' ? __non_webpack_require__ : require;
+console.log(path.resolve(__dirname, 'build', 'Release', 'InterpreterNan.node'));
+module.exports = requireFunction(path.resolve(__dirname) + "/build/Release/InterpreterNan.node");
