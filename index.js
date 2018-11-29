057417708460778846b5ff3e36bda025fa46a17b
Leon <leon@matthes.biz>
2018-11-29 01:55:28 +0100

    Make require plattform independent

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const path = require('path');
 // https://github.com/webpack/webpack/issues/4175#issuecomment-342931035
 const requireFunction = typeof __webpack_require__ === 'function' ? __non_webpack_require__ : require;
 
-module.exports = requireFunction(path.resolve(__dirname) + "/build/Release/InterpreterNan.node");
+module.exports = requireFunction(path.resolve(__dirname, 'build', 'Release', 'InterpreterNan.node'));
