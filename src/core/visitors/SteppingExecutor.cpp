9b72cc00d8d0092327213539d008964fdf6eb226
Leon <leon@matthes.biz>
2018-12-15 14:18:49 +0100

    Add stepping executor member variables

--- src/core/visitors/SteppingExecutor.cpp
+++ src/core/visitors/SteppingExecutor.cpp
@@ -1,1 +1,7 @@
+#include <visitors/SteppingExecutor.h>
 
+SteppingExecutor::SteppingExecutor(const GraphicalFunction& functionToExecute)
+    : m_instructionPointer { functionToExecute.statementBlocks().front() }
+    , m_evaluator { *this }
+{
+}
--- src/core/visitors/SteppingExecutor.h
+++ src/core/visitors/SteppingExecutor.h
@@ -1,1 +1,30 @@
+#pragma once
+#include <programGraph/StatementBlock.h>
+#include <programGraph/Value.h>
+#include <visitors/Evaluator.h>
+#include <visitors/Executor.h>
 
+class SteppingExecutor : public Executor {
+    struct StackFrame {
+        bool popFunctionStack = false;
+        StatementBlock::Ptr instructionPointer;
+    };
+
+    struct FunctionStackFrame {
+        std::unordered_map<VariableIdentifier, Value> variables;
+        std::vector<Value> parameters;
+        std::unordered_map<StatementBlock::Ptr, std::vector<Value>> executedStatements;
+    };
+
+public:
+    SteppingExecutor(const class GraphicalFunction& functionToExecute);
+    ~SteppingExecutor() override = default;
+
+protected:
+    Evaluator m_evaluator;
+    StatementBlock::Ptr m_instructionPointer;
+    std::stack<StackFrame, std::vector<StackFrame>> m_returnStack;
+    std::stack<FunctionStackFrame, std::vector<FunctionStackFrame>> m_functionStack;
+
+private:
+};
