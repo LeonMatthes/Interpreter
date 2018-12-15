b2aa7f91e85dfe2505a84c6f8e032c7368b82ca9
Leon <leon@matthes.biz>
2018-12-15 13:50:01 +0100

    Clang-format tests

--- src/tests/testVisitors/TestEvaluator.cpp
+++ src/tests/testVisitors/TestEvaluator.cpp
@@ -1,71 +1,69 @@
+#include <error/Error.h>
 #include <gtest/gtest.h>
-#include <visitors/Evaluator.h>
-#include <visitors/RecursiveExecutor.h>
-#include <testProgramGraph/MockFunction.h>
+#include <programGraph/FunctionBlock.h>
+#include <programGraph/GraphicalFunction.h>
+#include <programGraph/PrimitiveFunction.h>
 #include <programGraph/Value.h>
 #include <programGraph/ValueBlock.h>
-#include <programGraph/GraphicalFunction.h>
-#include <programGraph/FunctionBlock.h>
-#include <programGraph/PrimitiveFunction.h>
-#include <error/Error.h>
+#include <testProgramGraph/MockFunction.h>
+#include <visitors/Evaluator.h>
+#include <visitors/RecursiveExecutor.h>
 
-
-class MockExecutor : public RecursiveExecutor
-{
+class MockExecutor : public RecursiveExecutor {
 public:
     void visit(class GraphicalFunction& graphicalFunction) override
     {
         throw std::logic_error("The method or operation is not implemented.");
-	}
+    }
 
-        void visit(class FunctionBlock& functionBlock) override
-        {
-		throw std::logic_error("The method or operation is not implemented.");
-	}
+    void visit(class FunctionBlock& functionBlock) override
+    {
+        throw std::logic_error("The method or operation is not implemented.");
+    }
 
-        void visit(class Connection& connection) override
-        {
-		throw std::logic_error("The method or operation is not implemented.");
-	}
+    void visit(class Connection& connection) override
+    {
+        throw std::logic_error("The method or operation is not implemented.");
+    }
 
-        void visit(class ValueBlock& valueBlock) override
-        {
-		throw std::logic_error("The method or operation is not implemented.");
-	}
+    void visit(class ValueBlock& valueBlock) override
+    {
+        throw std::logic_error("The method or operation is not implemented.");
+    }
 
-        void visit(class PrimitiveFunction& primitiveFunction) override
-        {
-		throw std::logic_error("The method or operation is not implemented.");
-	}
+    void visit(class PrimitiveFunction& primitiveFunction) override
+    {
+        throw std::logic_error("The method or operation is not implemented.");
+    }
 };
 
-class TestEvaluator : public ::testing::Test
-{
+class TestEvaluator : public ::testing::Test {
 public:
-	MockExecutor m_executor;
-	Evaluator m_evaluator;
-	
-	TestEvaluator()
-		: m_executor(MockExecutor())
-		, m_evaluator(m_executor)
-	{}
+    MockExecutor m_executor;
+    Evaluator m_evaluator;
+
+    TestEvaluator()
+        : m_executor(MockExecutor())
+        , m_evaluator(m_executor)
+    {
+    }
+
 protected:
 private:
 };
 
-
 TEST_F(TestEvaluator, ValueBlock)
 {
-	Value value(false);
-	std::vector<Value> values({ value });
-	ValueBlock block(value);
-	EXPECT_EQ(values, block.accept(m_evaluator));
-} 
+    Value value(false);
+    std::vector<Value> values({ value });
+    ValueBlock block(value);
+    EXPECT_EQ(values, block.accept(m_evaluator));
+}
 
 TEST_F(TestEvaluator, PrimitiveFunction)
 {
-	m_evaluator.pushParameters({ Value(2.0), Value(1.0) });
-	EXPECT_EQ(Value(1.0), PrimitiveFunction::subtract.accept(m_evaluator).at(0));
+    m_evaluator.pushParameters({ Value(2.0), Value(1.0) });
+    EXPECT_EQ(Value(1.0), PrimitiveFunction::subtract.accept(m_evaluator).at(0));
 }
 
 TEST_F(TestEvaluator, Connection)
@@ -81,15 +79,15 @@ TEST_F(TestEvaluator, Connection)
 
 TEST_F(TestEvaluator, FunctionBlock)
 {
-	FunctionBlock block(PrimitiveFunction::subtract);
-	//Unconnected block uses default for input types
-	EXPECT_EQ(Value(0.0), block.accept(m_evaluator).at(0));
-	
-	ValueBlock::Ptr firstValue = std::make_shared<ValueBlock>(Value(2.0));
-	ValueBlock::Ptr secondValue = std::make_shared<ValueBlock>(Value(1.0));
-	block.setInputConnections({ Connection(firstValue, 0), Connection(secondValue, 0) });
-	EXPECT_EQ(Value(1.0), block.accept(m_evaluator).at(0));
+    FunctionBlock block(PrimitiveFunction::subtract);
+    //Unconnected block uses default for input types
+    EXPECT_EQ(Value(0.0), block.accept(m_evaluator).at(0));
 
-	block.setInputConnections({ Connection(firstValue, 0), Connection() });
-	EXPECT_EQ(Value(2.0), block.accept(m_evaluator).at(0));
+    ValueBlock::Ptr firstValue = std::make_shared<ValueBlock>(Value(2.0));
+    ValueBlock::Ptr secondValue = std::make_shared<ValueBlock>(Value(1.0));
+    block.setInputConnections({ Connection(firstValue, 0), Connection(secondValue, 0) });
+    EXPECT_EQ(Value(1.0), block.accept(m_evaluator).at(0));
+
+    block.setInputConnections({ Connection(firstValue, 0), Connection() });
+    EXPECT_EQ(Value(2.0), block.accept(m_evaluator).at(0));
 }
